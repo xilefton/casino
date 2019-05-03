80fad93592dc86523589b58910745672be0045f2
Sefarin <49129180+Sefarin@users.noreply.github.com>
2019-05-03 23:14:59 +0200

    Edited SlotMachine view

--- src/main/java/ch/bbbaden/casino/games/SlotMachineController.java
+++ src/main/java/ch/bbbaden/casino/games/SlotMachineController.java
@@ -2,16 +2,77 @@ package ch.bbbaden.casino.games;
 
 import ch.bbbaden.casino.Controller;
 import ch.bbbaden.casino.Model;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+
+import java.awt.event.ActionEvent;
 
 public class SlotMachineController implements Controller {
 
+    public AnchorPane anchorPaneSuperCherry;
+    public ImageView cherryCollectAction;
+    public ImageView threeStarSelection;
+    public ImageView betButton;
+    public ImageView gambleButton;
+    public ImageView mysteryButton;
+    public ImageView stopButton;
+    public ImageView holdLight;
+    public ImageView stepLight;
+    public ImageView winLight;
+    public ImageView bonusLightMystery;
+    public ImageView bonusLight;
+    public ImageView TwoFactorLight;
+    public ImageView ThreeFactorLight;
+    public ImageView FiveFactorLight;
+    public ScrollPane firstScrollPane;
+    public ScrollPane secondScrollPane;
+    public ScrollPane thirdScrollPane;
+    public HBox hBoxScrollBars;
+    public Label coins;
+    public Button addCoins;
+    public Button plusCoins;
+    public Button minusCoins;
     private SlotMachineModel slotMachineModel;
 
     public void update() {
-
+        coins.setText(slotMachineModel.getCoins()); 
     }
 
     public void initialize(Model model) {
         slotMachineModel = (SlotMachineModel) model;
+        update();
     }
+    public void OnClick(MouseEvent mouseEvent) {
+        getFruitsView();
+    }
+
+    private void getFruitsView() {
+        Image[] images = new Image[8];
+        ImageView[] pics = new ImageView[8];
+        final String[] imageNames = new String[]{"../images/supercherry/fruits/BELL.png", "../images/supercherry/fruits/CHERRY.png",
+                "../images/supercherry/fruits/GRAPES.png", "../images/supercherry/fruits/LEMON.png", "../images/supercherry/fruits/MELON.png",
+                "../images/supercherry/fruits/PEACH.png", "../images/supercherry/fruits/POTATO.png", "../images/supercherry/fruits/STAR.png",
+                "../images/supercherry/fruits/STRAWBERRY.png"};
+
+        for (int i = 0; i < 8; i++) {
+            images[i] = new Image(getClass().getResourceAsStream(imageNames[i]));
+            pics[i] = new ImageView(images[i]);
+            pics[i].setFitWidth(100);
+            pics[i].setPreserveRatio(true);
+            hBoxScrollBars.getChildren().add(pics[i]);
+            firstScrollPane.setContent(hBoxScrollBars);
+            secondScrollPane.setContent(hBoxScrollBars);
+            thirdScrollPane.setContent(hBoxScrollBars);
+
+        }
+    }
+
+
 }
+
--- src/main/java/ch/bbbaden/casino/games/SlotMachineModel.java
+++ src/main/java/ch/bbbaden/casino/games/SlotMachineModel.java
@@ -3,8 +3,24 @@ package ch.bbbaden.casino.games;
 import ch.bbbaden.casino.Model;
 import ch.bbbaden.casino.NormalUser;
 
+import java.sql.SQLException;
+
+
 public class SlotMachineModel extends Game {
+    private NormalUser normalUser;
     public SlotMachineModel(NormalUser normalUser) {
         super("/fxml/SlotMachine.fxml", "Super Cherry", "/images/SuperCherry_Logo.png", normalUser);
+        this.normalUser = normalUser;
+
+    }
+
+    public String getCoins() {
+        try {
+            return Integer.toString(normalUser.getCoins());
+        } catch (SQLException e) {
+            System.err.println(e);
+        } return null;
+    }
+    public void addCoins() {
     }
 }
