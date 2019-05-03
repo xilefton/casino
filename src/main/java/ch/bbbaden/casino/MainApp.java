f2afc469a5127900883682eaeaed5a046e553087
Sefarin <49129180+Sefarin@users.noreply.github.com>
2019-05-03 11:15:19 +0200

    SlotMachine view created

--- src/main/java/ch/bbbaden/casino/MainApp.java
+++ src/main/java/ch/bbbaden/casino/MainApp.java
@@ -1,9 +1,12 @@
 package ch.bbbaden.casino;
 
+import ch.bbbaden.casino.games.SlotMachineModel;
 import ch.bbbaden.casino.scenes.StartModel;
 import javafx.application.Application;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import java.sql.SQLException;
 
 public class MainApp extends Application {
 
@@ -12,8 +15,11 @@ public class MainApp extends Application {
     }
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws SQLException {
         Font.loadFont(getClass().getResourceAsStream("/fonts/casino.ttf"), 20);
-        new ModelManager(stage).setModel(new StartModel());
+        //new ModelManager(stage).setModel(new StartModel());
+        NormalUser normalUser = new NormalUser();
+        normalUser.login("SEVERIN", "nireves");
+        new ModelManager(stage).setModel(new SlotMachineModel(normalUser));
     }
 }
