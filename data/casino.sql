0f5ef4ec86f8d90a33c194e504aa811daa465f35
XelaaleX1234 <32978721+Xelaalex1234@users.noreply.github.com>
2019-05-06 16:11:07 +0200

    added updated dump

--- data/casino.sql
+++ data/casino.sql
@@ -1,18 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 06. Mai 2019 um 16:09
+-- Server-Version: 10.1.37-MariaDB
+-- PHP-Version: 7.3.0
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Datenbank: `casino`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `adminusers`
+--
+
 CREATE TABLE `adminusers` (
   `ID` int(11) NOT NULL,
   `username` text NOT NULL,
   `password` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `games`
+--
 
 CREATE TABLE `games` (
   `ID` int(11) NOT NULL,
@@ -22,8 +48,14 @@ CREATE TABLE `games` (
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `normalusers`
+--
+
 CREATE TABLE `normalusers` (
-  `ID` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `username` text NOT NULL,
   `password` text NOT NULL,
   `coins` int(11) NOT NULL,
@@ -31,23 +63,50 @@ CREATE TABLE `normalusers` (
   `bet` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `adminusers`
+--
 ALTER TABLE `adminusers`
   ADD PRIMARY KEY (`ID`);
 
+--
+-- Indizes für die Tabelle `games`
+--
 ALTER TABLE `games`
   ADD PRIMARY KEY (`ID`);
 
+--
+-- Indizes für die Tabelle `normalusers`
+--
 ALTER TABLE `normalusers`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `adminusers`
+--
 ALTER TABLE `adminusers`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT für Tabelle `games`
+--
 ALTER TABLE `games`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
+--
+-- AUTO_INCREMENT für Tabelle `normalusers`
+--
 ALTER TABLE `normalusers`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
